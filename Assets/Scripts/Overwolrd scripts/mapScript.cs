4c4179779d8191b2f260c9578a1222f08328bf9a
Neoteuthis <Neoteuthis@users.noreply.github.com>
2019-03-14 13:16:21 -0400

    added moveable map to overworld

--- Assets/Scripts/Overwolrd scripts/mapScript.cs
+++ Assets/Scripts/Overwolrd scripts/mapScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class mapScript : MonoBehaviour
 {
+    float mapSpeed = 0.3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,16 @@ public class mapScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Drag");
+            float lr = mapSpeed * Input.GetAxis("Mouse X");
+            float ud = mapSpeed * Input.GetAxis("Mouse Y");
+            transform.Translate(lr, ud, 0);
+        }
+        else
+        {
+            Debug.Log("Drop");
+        }
     }
 }
