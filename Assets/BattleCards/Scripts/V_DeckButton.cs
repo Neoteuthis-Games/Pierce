cab71a7330de4593a5ffccbdd9b436d07b5f09c5
Neoteuthis <Neoteuthis@users.noreply.github.com>
2019-03-14 12:44:55 -0400

    added overworld and activated campaign button

--- Assets/BattleCards/Scripts/V_DeckButton.cs
+++ Assets/BattleCards/Scripts/V_DeckButton.cs
@@ -20,15 +20,29 @@ public class V_DeckButton : MonoBehaviour {
 
 	public Text deckNameText;
 	public int theDeck;
+    public static bool campaignEntry = false;
+    public static bool arenaEntry = false;
 	[HideInInspector] public V_Menu mainMenu;	// Used for accessing the deck editor...
 
 	public void Start(){
 		deckNameText.text = mainMenu.deckEdit.decks [theDeck].deckName;
 	}
-
-	public void Click(){
-		V_PlayerHandler.selectedDeck = theDeck;
-        V_GameManager.ActiveState = V_GameManager.Gamestate.InGame;
-		mainMenu.GoTo ("InGame");
-	}
+    public void campaign()
+    {
+        campaignEntry = true;
+    }
+    public void Click()
+    {
+        V_PlayerHandler.selectedDeck = theDeck;
+        if (campaignEntry == false)
+        {
+            V_GameManager.ActiveState = V_GameManager.Gamestate.InGame;
+            mainMenu.GoTo("InGame");
+        }
+        else
+        {
+            V_GameManager.ActiveState = V_GameManager.Gamestate.Overworld;
+            mainMenu.GoTo("Overworld_Airam");
+        }
+    }
 }
